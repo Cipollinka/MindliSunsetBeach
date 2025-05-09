a878441c0a6283dceb3cac4057b2be64885953b1
Cipollinka <65620481+Cipollinka@users.noreply.github.com>
2025-05-09 15:16:05 +0300

    fix date

--- src/AppManager/Params.js
+++ src/AppManager/Params.js
@@ -1,5 +1,5 @@
 export const keyPush = 'ea5d8dbd-ca0f-41cc-bfef-765e81c273a7';
 export const appID = 'id6745400147';
 export const keyApps = 'v7xCW2oiGJ5JauPXwWiS5W';
-export const targetDate = '2025-05-11';
+export const targetDate = '2025-05-19';
 export const bodyLin = 'https://superb-grandiose-domination.space/adxfXCmW';
